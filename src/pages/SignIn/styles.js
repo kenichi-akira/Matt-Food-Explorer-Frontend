a977c30127936d1ed56d20f19a3ade404d934cfb
kenichi-akira <161388141+kenichi-akira@users.noreply.github.com>
2024-11-14 03:55:44 -0300

    Update styles.js

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -4,98 +4,145 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     
-    max-width: 110.6rem;
+    width: 100%;
+    min-width: 35rem;
     height: 100vh;
     
-    margin: auto;
-    padding: 14.2rem 4rem;
-    
-    justify-content: space-between;
-    align-items: center;
-
-    animation: puff-in-center 0.7s cubic-bezier(0.470, 0.000, 0.745, 0.715) both;
-
-    @keyframes puff-in-center {
-        0% {
-            transform: scale(2);
-            filter: blur(4px);
-            opacity: 0;
-        }
-        100% {
-            transform: scale(1);
-            filter: blur(0px);
-            opacity: 1;
-        }
-    }
-
-    @media only screen and (min-width: 768px) {
-        flex-direction: row;
-    }
+    overflow: auto;
+    overflow: overlay; 
 `;
 
-export const Form = styled.form`
+export const Content = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: left;
     
-    max-width: 47.6rem;
     width: 100%;
-    height: 54rem;
-    
-    padding: 6.4rem;
-    border-radius: 1.6rem;
+    max-width: 121.2rem;
+    margin: auto;
+    padding: 3.5rem 4rem;
 
-    background-color: ${({ theme }) => theme.COLORS.RED};
-    -webkit-box-shadow: 0px 0px 10px 5px #193746; 
-    box-shadow: 0px 0px 10px 5px #193746;
+    font-family: 'Poppins', sans-serif;
 
-    > h2 {
-        font-family: 'Poppins', sans-serif;
+    .swiper {
+        margin-bottom: 6rem;
+    }
+
+    p {
         font-size: 3.2rem;
-        font-weight: 500;
-        line-height: 6.8rem;
-        text-align: center;
+        margin-bottom: 3rem;
+    }
+    
+    .swiper-slide {
+        /* Center slide text vertically */
+        display: -webkit-box;
+        display: -ms-flexbox;
+        display: -webkit-flex;
+        display: flex;
+        -webkit-box-pack: center;
+        -ms-flex-pack: center;
+        -webkit-justify-content: center;
+        justify-content: center;
+        -webkit-box-align: center;
+        -ms-flex-align: center;
+        -webkit-align-items: center;
+        align-items: center;
+    }
+    
+    .swiper-button-next,
+    .swiper-button-prev {
+        width: 9rem;
+        height: 51.2rem;
+        margin: -25.6rem -1rem;
         
-        margin-bottom: 3.2rem;
+        color: ${({ theme }) => theme.COLORS.WHITE};
+        font-weight: bolder;
+        mask-image: none;
     }
 
-    > a {
-        margin-top: 3.2rem;
-        
-        text-align: center;
-        color: ${({ theme }) => theme.COLORS.RED};
+    .swiper-button-next:hover,
+    .swiper-button-prev:hover {
+	    animation: scale-up-center 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
     }
 
-    > .inputs {
-        margin-bottom: 3.2rem;
+    .swiper-button-prev {
+        background: linear-gradient(to left, transparent 0%, ${({ theme }) => theme.COLORS.BACKGROUND_900} 100% 0%, transparent 100%);
     }
 
-    > .inputs p {
-        font-size: 1.6rem;
-        margin-bottom: 0.8rem;
-        color: ${({ theme }) => theme.COLORS.GRAY_100};
+    .swiper-button-next {
+        background: linear-gradient(to right, transparent 0%, ${({ theme }) => theme.COLORS.BACKGROUND_900} 100% 0%, transparent 100%);
+    }
+
+    @keyframes scale-up-center {
+        0% {
+            transform: scale(1);
+        }
+        100% {
+            transform: scale(1.2);
+        }
     }
 `;
 
-export const Logo = styled.div`
-    display: flex;
-    align-items: center;
-    color: ${({ theme }) => theme.COLORS.RED};
-    margin-bottom: 4rem;
+export const Banner = styled.div`
+    text-align: center;
+    justify-content: space-between;
+    font-family: 'Poppins', sans-serif;
+
+    margin-bottom: 6.2rem;
+
+    .banner {
+        background: linear-gradient(180deg, ${({ theme }) => theme.COLORS.BLUE_400} 0%, ${({ theme }) => theme.COLORS.BLUE_500} 100%);
+        border-radius: 0.8rem;
+        position: relative;
+        -webkit-box-shadow: 0px 0px 10px 5px #193746; 
+        box-shadow: 0px 0px 10px 5px #193746;
+    }
+
+    .title {
+        padding: 1rem;
+    }
+
+    img {
+        width: 100%;
+        margin-bottom: -2.7rem;
+        z-index: -1;
+    }
 
     h1 {
-        font-size: clamp(3rem, 3rem + 1.5vw, 4.248rem);
-        white-space: nowrap;
-    }
-    
-    .logo {
-        display: flex;
-        gap: 1.9rem;
+        line-height: 140%;
+        font-size: clamp(1rem, 6vw, 4rem);
+        font-weight: 500;
+
+        margin-bottom: 0.8rem;
     }
 
-    svg {
-        height: 5rem;
-        width: 5rem;
+    span {
+        font-size: clamp(1rem, 2.5vw, 1.6rem);
+    }
+
+    @media only screen and (min-width: 1060px) {
+        position: relative;
+        margin-top: 16.4rem;
+
+        .banner {
+            display: flex;
+            justify-content: end;
+        }
+        
+        .title {
+            width: 54rem;
+            height: 26rem;
+
+            padding: 8.7rem 4.6rem 0 0;
+
+            text-align: right;
+        }
+
+        img {
+            width: 65.6rem;
+            position: absolute;
+            bottom: 1.3rem;
+            z-index: 1;
+            left: -5rem;
+        }
     }
 `;
