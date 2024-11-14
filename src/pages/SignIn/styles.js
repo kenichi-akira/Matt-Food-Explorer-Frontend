d0ac9403a50da690e2ed840ea1679d94bdb13f28
kenichi-akira <161388141+kenichi-akira@users.noreply.github.com>
2024-11-14 04:01:25 -0300

    Update styles.js

--- src/pages/SignIn/styles.js
+++ src/pages/SignIn/styles.js
@@ -1,148 +1,59 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+
+  > div {
+    padding-block: 5rem;
+    max-width: 47.6rem;
+
     display: flex;
-    flex-direction: column;
-    
-    width: 100%;
-    min-width: 35rem;
-    height: 100vh;
-    
-    overflow: auto;
-    overflow: overlay; 
-`;
-
-export const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    
-    width: 100%;
-    max-width: 121.2rem;
-    margin: auto;
-    padding: 3.5rem 4rem;
-
-    font-family: 'Poppins', sans-serif;
-
-    .swiper {
-        margin-bottom: 6rem;
-    }
-
-    p {
-        font-size: 3.2rem;
-        margin-bottom: 3rem;
-    }
-    
-    .swiper-slide {
-        /* Center slide text vertically */
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: -webkit-flex;
-        display: flex;
-        -webkit-box-pack: center;
-        -ms-flex-pack: center;
-        -webkit-justify-content: center;
-        justify-content: center;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        -webkit-align-items: center;
-        align-items: center;
-    }
-    
-    .swiper-button-next,
-    .swiper-button-prev {
-        width: 9rem;
-        height: 51.2rem;
-        margin: -25.6rem -1rem;
-        
-        color: ${({ theme }) => theme.COLORS.WHITE};
-        font-weight: bolder;
-        mask-image: none;
-    }
-
-    .swiper-button-next:hover,
-    .swiper-button-prev:hover {
-	    animation: scale-up-center 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both;
-    }
-
-    .swiper-button-prev {
-        background: linear-gradient(to left, transparent 0%, ${({ theme }) => theme.COLORS.BACKGROUND_900} 100% 0%, transparent 100%);
-    }
-
-    .swiper-button-next {
-        background: linear-gradient(to right, transparent 0%, ${({ theme }) => theme.COLORS.BACKGROUND_900} 100% 0%, transparent 100%);
-    }
-
-    @keyframes scale-up-center {
-        0% {
-            transform: scale(1);
-        }
-        100% {
-            transform: scale(1.2);
-        }
-    }
-`;
-
-export const Banner = styled.div`
-    text-align: center;
-    justify-content: space-between;
-    font-family: 'Poppins', sans-serif;
-
-    margin-bottom: 6.2rem;
-
-    .banner {
-        background: linear-gradient(180deg, ${({ theme }) => theme.COLORS.BLUE_400} 0%, ${({ theme }) => theme.COLORS.BLUE_500} 100%);
-        border-radius: 0.8rem;
-        position: relative;
-        -webkit-box-shadow: 0px 0px 10px 5px #193746; 
-        box-shadow: 0px 0px 10px 5px #193746;
-    }
-
-    .title {
-        padding: 1rem;
-    }
-
-    img {
-        width: 100%;
-        margin-bottom: -2.7rem;
-        z-index: -1;
-    }
+    align-items: center;
+    justify-content: center; 
+    gap: 2rem;   
 
     h1 {
-        line-height: 140%;
-        font-size: clamp(1rem, 6vw, 4rem);
-        font-weight: 500;
-
-        margin-bottom: 0.8rem;
+      font-size: clamp(3.2rem, 1rem + 5vw, 4.2rem);
     }
+  }
 
-    span {
-        font-size: clamp(1rem, 2.5vw, 1.6rem);
-    }
+  @media (min-width: 768px) {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-inline: auto;
+    width: 120rem;
+  }
+`
 
-    @media only screen and (min-width: 1060px) {
-        position: relative;
-        margin-top: 16.4rem;
+export const Form = styled.form`
+  margin-inline: auto;
+  padding-block: 6.4rem;
+  padding-inline: 2.4rem;
+  border-radius: 1.6rem;
+  background-color: ${({ theme }) => theme.COLORS.BACKGROUND_600};
+  max-width: 47.6rem;
+  height: fit-content;
 
-        .banner {
-            display: flex;
-            justify-content: end;
-        }
-        
-        .title {
-            width: 54rem;
-            height: 26rem;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  gap: 3.2rem;
 
-            padding: 8.7rem 4.6rem 0 0;
+> legend {
+  font-size: 3.2rem;
+  text-align: center;
+}
 
-            text-align: right;
-        }
+> a {
+  color: ${({ theme }) => theme.COLORS.WHITE};
+  font-size: 1.4rem;
+  text-align: center;
+}
 
-        img {
-            width: 65.6rem;
-            position: absolute;
-            bottom: 1.3rem;
-            z-index: 1;
-            left: -5rem;
-        }
-    }
-`;
+@media (min-width: 768px) {
+  margin-inline: 0;
+  padding: 6.4rem;
+}
+`
